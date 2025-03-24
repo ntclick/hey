fc06939407ba8678e431aecba9bfd701d46962a2
Yoginth <hey@yoginth.com>
2025-03-24 20:31:16 +0530

    refactor: Simplify health router by removing unnecessary response header setting

--- packages/rpc/src/routers/health.ts
+++ packages/rpc/src/routers/health.ts
@@ -1,8 +1,7 @@
 import { publicProcedure, router } from "../trpc";
 
 export const healthRouter = router({
-  health: publicProcedure.query(({ ctx }) => {
-    ctx.res.setHeader("Gm-Control-Allow-Origin", "*");
+  health: publicProcedure.query(() => {
     return { ping: "pong" };
   })
 });
