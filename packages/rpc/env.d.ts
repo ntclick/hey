dfe188997eee642356cd61d08f538c489dd6a273
Yoginth <hey@yoginth.com>
2025-03-26 22:42:22 +0530

    chore: Remove SECRET variable from environment configuration

--- packages/rpc/env.d.ts
+++ packages/rpc/env.d.ts
@@ -3,7 +3,6 @@ declare namespace NodeJS {
     NEXT_PUBLIC_LENS_NETWORK: string;
     DATABASE_URL: string;
     PRIVATE_KEY: string;
-    SECRET: string;
     EVER_ACCESS_KEY: string;
     EVER_ACCESS_SECRET: string;
   }
