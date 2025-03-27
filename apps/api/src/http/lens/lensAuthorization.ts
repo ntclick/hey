5b0de8b7ea7011d3229e199b0f3c036fcd0cde91
Yoginth <hey@yoginth.com>
2025-03-27 18:14:21 +0530

    refactor: Simplify lensAuthorization function by removing database dependency and hardcoding response values

--- apps/api/src/http/lens/lensAuthorization.ts
+++ apps/api/src/http/lens/lensAuthorization.ts
@@ -1,26 +1,13 @@
-import { Errors } from "@hey/data/errors";
-import { PermissionId } from "@hey/data/permissions";
 import type { Request, Response } from "express";
 import {
   CACHE_AGE_1_DAY,
   VERIFICATION_ENDPOINT
 } from "../../helpers/constants";
-import prisma from "../../prisma/client";
 
-export const lensAuthorization = async (req: Request, res: Response) => {
-  const { account } = req.body;
-
-  try {
-    const suspended = await prisma.accountPermission.findFirst({
-      where: { permissionId: PermissionId.Suspended, accountAddress: account }
-    });
-
-    return res.setHeader("Cache-Control", CACHE_AGE_1_DAY).json({
-      allowed: true,
-      sponsored: !suspended?.enabled,
-      appVerificationEndpoint: VERIFICATION_ENDPOINT
-    });
-  } catch {
-    return res.json({ error: Errors.SomethingWentWrong });
-  }
+export const lensAuthorization = async (_: Request, res: Response) => {
+  return res.setHeader("Cache-Control", CACHE_AGE_1_DAY).json({
+    allowed: true,
+    sponsored: true,
+    appVerificationEndpoint: VERIFICATION_ENDPOINT
+  });
 };
