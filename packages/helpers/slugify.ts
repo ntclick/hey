fa5f5b3bb687441195323e3fb7c075842bb8f03e
Yoginth <hey@yoginth.com>
2025-03-30 14:47:18 +0530

    chore: Remove unused sha256 and slugify helper functions

--- packages/helpers/slugify.ts
+++
@@ -1,9 +0,0 @@
-const slugify = (text: string): string =>
-  text
-    .trim() // Trim leading and trailing spaces
-    .toLowerCase()
-    .replace(/[^\d a-z-]/g, "") // Remove invalid chars
-    .replace(/\s+/g, "-") // Replace spaces with -
-    .replace(/-+/g, "-"); // Replace multiple - with single -
-
-export default slugify;
