c2d67f8d17128ce8e645095c6669554d1785b07c
Yoginth <hey@yoginth.com>
2025-04-02 20:49:42 +0530

    fix: Update LENS_MAINNET_RPCS endpoint for improved connectivity

--- packages/data/rpcs.ts
+++ packages/data/rpcs.ts
@@ -1,5 +1,5 @@
 export const LENS_TESTNET_RPCS = [
-  "https://lens-sepolia.g.alchemy.com/v2/Xx-4a1SyWtS9U4h0cEuRmvgYtGeVOlv7",
-  "https://rpc.yoginth.com"
+  "https://lens-sepolia.g.alchemy.com/v2/Xx-4a1SyWtS9U4h0cEuRmvgYtGeVOlv7"
 ];
-export const LENS_MAINNET_RPCS = ["https://api.lens.matterhosted.dev"];
+
+export const LENS_MAINNET_RPCS = ["https://rpc.lens.xyz"];
