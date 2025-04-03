001b803a61627302b5c43ac55b28350541ab6e09
Yoginth <yoginth@hey.com>
2025-04-03 10:52:20 +0530

    refactor: Remove CI/CD configuration files for Pierre

--- .pierre/ci/deploy.ts
+++ .pierre/ci/deploy.ts
@@ -1,5 +1,7 @@
 import { run } from "pierre";
 
+export const label = "Deploy";
+
 export default async ({ branch }) => {
   if (branch.name !== "main") {
     throw new Error("Only main branch is allowed to deploy");
