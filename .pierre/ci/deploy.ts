fb89d52c51971f707464433cff69a01fafe96369
Yoginth <yoginth@hey.com>
2025-04-03 10:58:26 +0530

    feat: Configure AWS credentials and region in deployment script

--- .pierre/ci/deploy.ts
+++ .pierre/ci/deploy.ts
@@ -10,4 +10,18 @@ export default async ({ branch }) => {
   await run("cd apps/web && pnpm build", {
     label: "Building web"
   });
+
+  await run(
+    "aws configure set aws_access_key_id ${{ process.env.AWS_ACCESS_KEY_ID }}",
+    { label: "Configuring Access Key" }
+  );
+
+  await run(
+    "aws configure set aws_secret_access_key ${{ process.env.AWS_SECRET_ACCESS_KEY }}",
+    { label: "Configuring Secret Access Key" }
+  );
+
+  await run("aws configure set default.region us-east-1", {
+    label: "Configuring Region"
+  });
 };
