5aaeb71b869809e336da313fcb1a6e16db34c065
Yoginth <yoginth@hey.com>
2025-04-05 10:29:19 +0530

    feat: Add Tailwind CSS plugins for aspect ratio and forms, and refine Input component styling

--- apps/web/src/components/Shared/UI/Input.tsx
+++ apps/web/src/components/Shared/UI/Input.tsx
@@ -65,7 +65,7 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(function Input(
             className={cn(
               { "placeholder:text-red-500": error },
               prefix ? "rounded-r-xl" : "rounded-xl",
-              "peer w-full border-none bg-transparent px-3 py-2 outline-hidden focus:ring-0",
+              "peer w-full border-none bg-transparent outline-hidden focus:ring-0",
               className
             )}
             id={id}
