10fa0951b8d549b3e05541b4730613836fc2e043
Yoginth <yoginth@hey.com>
2025-04-08 20:46:37 +0530

    Update vite config

--- apps/web/vite.config.mjs
+++ apps/web/vite.config.mjs
@@ -14,5 +14,64 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src")
     }
+  },
+  build: {
+    target: "esnext",
+    cssMinify: "lightningcss",
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (/\.woff2$/.test(assetInfo.name ?? "")) {
+            return "assets/fonts/[name][extname]";
+          }
+
+          return "assets/[name]-[hash][extname]";
+        },
+        manualChunks: {
+          wevm: ["viem", "wagmi", "family"],
+          indexer: ["@hey/indexer"],
+          react: [
+            "react",
+            "react-dom",
+            "react-virtuoso",
+            "react-easy-crop",
+            "react-hook-form",
+            "react-hot-toast",
+            "react-router",
+            "react-tracked"
+          ],
+          editor: [
+            "react-markdown",
+            "strip-markdown",
+            "unified",
+            "prosekit",
+            "rehype-parse",
+            "rehype-remark",
+            "remark-breaks",
+            "remark-html",
+            "remark-linkify-regex",
+            "remark-stringify"
+          ],
+          ui: [
+            "@heroicons/react",
+            "@headlessui/react",
+            "@radix-ui/react-hover-card",
+            "@radix-ui/react-slider",
+            "@radix-ui/react-tooltip",
+            "@uidotdev/usehooks"
+          ],
+          aws: ["@aws-sdk/client-s3", "@aws-sdk/lib-storage"],
+          media: ["plyr-react", "@livepeer/react", "browser-image-compression"],
+          misc: [
+            "@lens-chain/storage-client",
+            "@lens-protocol/metadata",
+            "@apollo/client",
+            "zustand",
+            "tailwind-merge",
+            "zod"
+          ]
+        }
+      }
+    }
   }
 });
