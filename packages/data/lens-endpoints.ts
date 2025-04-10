22580289cea0f4cd5141b9ef46af7477d4ef935f
Yoginth <yoginth@hey.com>
2025-04-10 20:06:56 +0530

    Fix staging url

--- packages/data/lens-endpoints.ts
+++ packages/data/lens-endpoints.ts
@@ -1,7 +1,7 @@
 enum LensEndpoint {
   Mainnet = "https://api.lens.xyz/graphql",
   Testnet = "https://api.testnet.lens.xyz/graphql",
-  Staging = "https://api.staging.lens.xyz/graphql"
+  Staging = "https://api.staging.lens.dev/graphql"
 }
 
 export default LensEndpoint;
