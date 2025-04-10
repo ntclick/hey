bb19cfb7695f79829985545f334a7d09af8f3897
Yoginth <yoginth@hey.com>
2025-04-10 10:47:17 +0530

    Reorder RPC URLs in rpcs.ts for consistency and clarity

--- packages/data/rpcs.ts
+++ packages/data/rpcs.ts
@@ -1,9 +1,9 @@
 export const LENS_TESTNET_RPCS = [
-  "https://lens-sepolia.g.alchemy.com/v2/N_HuqeYE3mr_enxw-BGFI2rOm1U7bhGy",
-  "https://rpc.testnet.lens.dev"
+  "https://rpc.testnet.lens.dev",
+  "https://lens-sepolia.g.alchemy.com/v2/N_HuqeYE3mr_enxw-BGFI2rOm1U7bhGy"
 ];
 
 export const LENS_MAINNET_RPCS = [
-  "https://lens-mainnet.g.alchemy.com/v2/N_HuqeYE3mr_enxw-BGFI2rOm1U7bhGy",
-  "https://rpc.lens.xyz"
+  "https://rpc.lens.xyz",
+  "https://lens-mainnet.g.alchemy.com/v2/N_HuqeYE3mr_enxw-BGFI2rOm1U7bhGy"
 ];
