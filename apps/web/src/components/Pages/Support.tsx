6209034016bf5bc18f817034bca6a5fb33bbe678
Yoginth <69431456+bigint@users.noreply.github.com>
2025-04-13 00:22:28 +0530

    refactor: replace unordered list with a flexbox layout for support links in Support component

--- apps/web/src/components/Pages/Support.tsx
+++ apps/web/src/components/Pages/Support.tsx
@@ -13,28 +13,18 @@ const Support = () => {
             <Link to="mailto:support@hey.xyz">support@hey.xyz</Link> with a
             detailed description of your issue and how we can assist you.
           </p>
-          <ul className="my-5 space-y-2">
-            <li>
-              <Link to="/guidelines">Community Guidelines</Link>
-            </li>
-            <li>
-              <Link to="/terms">Terms of Service</Link>
-            </li>
-            <li>
-              <Link to="/privacy">Hey Privacy Policy</Link>
-            </li>
-            <li>
-              <Link
-                to="https://www.lens.xyz/legal/lens.xyz-privacy-policy.pdf"
-                target="_blank"
-              >
-                Lens Privacy Policy
-              </Link>
-            </li>
-            <li>
-              <Link to="/copyright">Copyright Policy</Link>
-            </li>
-          </ul>
+          <div className="my-5 flex flex-col space-y-2">
+            <Link to="/guidelines">Community Guidelines</Link>
+            <Link to="/terms">Terms of Service</Link>
+            <Link to="/privacy">Hey Privacy Policy</Link>
+            <Link
+              to="https://www.lens.xyz/legal/lens.xyz-privacy-policy.pdf"
+              target="_blank"
+            >
+              Lens Privacy Policy
+            </Link>
+            <Link to="/copyright">Copyright Policy</Link>
+          </div>
           <p className="text-gray-500 text-sm">
             Send any legal requests to{" "}
             <Link to="mailto:legal@hey.xyz">legal@hey.xyz</Link>
