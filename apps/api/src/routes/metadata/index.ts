5dd714ad3b05678bf7d1de382f76ccb48c25eb31
Yoginth <69431456+bigint@users.noreply.github.com>
2025-04-14 12:41:31 +0530

    refactor: simplify /sts route by removing zod validation for cleaner code

--- apps/api/src/routes/metadata/index.ts
+++ apps/api/src/routes/metadata/index.ts
@@ -1,16 +1,9 @@
-import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import rateLimiter from "src/middlewares/rateLimiter";
-import { z } from "zod";
 import getSTS from "./getSTS";
 
 const app = new Hono();
 
-app.get(
-  "/sts",
-  rateLimiter({ requests: 50 }),
-  zValidator("json", z.object({ record: z.boolean() })),
-  getSTS
-);
+app.get("/sts", rateLimiter({ requests: 50 }), getSTS);
 
 export default app;
