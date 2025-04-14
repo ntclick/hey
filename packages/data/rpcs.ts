8440c7877359e1d696765f031cc147b0e98a5b94
Yoginth <69431456+bigint@users.noreply.github.com>
2025-04-15 00:14:35 +0530

    refactor: reorder and update LENS_TESTNET_RPCS for consistency with mainnet configuration

--- packages/data/rpcs.ts
+++ packages/data/rpcs.ts
@@ -1,10 +1,10 @@
-export const LENS_TESTNET_RPCS = [
-  "https://rpc.testnet.lens.dev",
-  "https://lens-sepolia.g.alchemy.com/v2/N_HuqeYE3mr_enxw-BGFI2rOm1U7bhGy"
-];
-
 export const LENS_MAINNET_RPCS = [
   "https://rpc.lens.xyz",
   "https://api.lens.matterhosted.dev",
   "https://lens-mainnet.g.alchemy.com/v2/N_HuqeYE3mr_enxw-BGFI2rOm1U7bhGy"
+];
+
+export const LENS_TESTNET_RPCS = [
+  "https://rpc.testnet.lens.dev",
+  "https://lens-sepolia.g.alchemy.com/v2/N_HuqeYE3mr_enxw-BGFI2rOm1U7bhGy"
 ];
