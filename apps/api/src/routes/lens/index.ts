4b1e71fcfe6ff733ff696d4f2e8bea9d614a554e
Yoginth <69431456+bigint@users.noreply.github.com>
2025-04-14 19:06:59 +0530

    refactor: implement Zod validation for authorization and verification routes in lens API for improved data integrity

--- apps/api/src/routes/lens/index.ts
+++ apps/api/src/routes/lens/index.ts
@@ -1,10 +1,37 @@
+import { Regex } from "@hey/data/regex";
+import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
+import { z } from "zod";
 import lensAuthorization from "./lensAuthorization";
 import lensVerification from "./lensVerification";
 
 const app = new Hono();
 
-app.post("/authorization", lensAuthorization);
-app.post("/verification", lensVerification);
+app.post(
+  "/authorization",
+  zValidator(
+    "json",
+    z.object({
+      account: z.string().regex(Regex.evmAddress),
+      signedBy: z.string().regex(Regex.evmAddress)
+    })
+  ),
+  lensAuthorization
+);
+
+app.post(
+  "/verification",
+  zValidator(
+    "json",
+    z.object({
+      nonce: z.string(),
+      deadline: z.string(),
+      operation: z.string(),
+      validator: z.string().regex(Regex.evmAddress),
+      account: z.string().regex(Regex.evmAddress)
+    })
+  ),
+  lensVerification
+);
 
 export default app;
