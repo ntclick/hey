ac5c11a42421d0aecf81d12c8fe5a1a714e59940
Yoginth <69431456+bigint@users.noreply.github.com>
2025-04-14 23:18:03 +0530

    chore: update dependencies in package.json and pnpm-lock.yaml, add LENS_DATABASE_URL to environment configuration

--- apps/api/env.d.ts
+++ apps/api/env.d.ts
@@ -2,6 +2,7 @@ declare namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_LENS_NETWORK: string;
     DATABASE_URL: string;
+    LENS_DATABASE_URL: string;
     REDIS_URL: string;
     PRIVATE_KEY: string;
     EVER_ACCESS_KEY: string;
