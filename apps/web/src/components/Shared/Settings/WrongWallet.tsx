bdbc4d75b73112df6d9896d6af935e7db4700578
Yoginth <69431456+bigint@users.noreply.github.com>
2025-04-16 12:58:43 +0530

    refactor: replace Card component with a div in WrongWallet for simplified structure

--- apps/web/src/components/Shared/Settings/WrongWallet.tsx
+++ apps/web/src/components/Shared/Settings/WrongWallet.tsx
@@ -1,9 +1,9 @@
-import { Card, H5 } from "@/components/Shared/UI";
+import { H5 } from "@/components/Shared/UI";
 import WalletSelector from "../Auth/WalletSelector";
 
 const WrongWallet = () => {
   return (
-    <Card className="linkify space-y-2 p-5">
+    <div className="space-y-2 p-5">
       <div className="space-y-3 pb-2">
         <H5>Switch to correct wallet</H5>
         <p>
@@ -12,7 +12,7 @@ const WrongWallet = () => {
         </p>
       </div>
       <WalletSelector />
-    </Card>
+    </div>
   );
 };
 
