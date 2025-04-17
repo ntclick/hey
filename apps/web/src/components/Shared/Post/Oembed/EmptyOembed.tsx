2c6f4536e7ea23f4d161332da6aa71f11d15dea5
Yoginth <--global>
2025-04-17 13:38:42 +0530

    refactor: streamline EmptyOembed component structure for improved readability

--- apps/web/src/components/Shared/Post/Oembed/EmptyOembed.tsx
+++ apps/web/src/components/Shared/Post/Oembed/EmptyOembed.tsx
@@ -8,19 +8,17 @@ interface EmptyOembedProps {
 
 const EmptyOembed = ({ url, hideLoader = false }: EmptyOembedProps) => {
   return (
-    <div className="mt-4 w-full text-sm md:w-4/6">
-      <Card className="truncate p-5" forceRounded>
-        <div className="flex flex-col gap-y-1">
-          <div className="flex items-center space-x-1.5">
-            <LinkIcon className="size-4 text-gray-500 dark:text-gray-200" />
-            <b className="max-w-sm truncate">{url}</b>
-          </div>
-          {!hideLoader && (
-            <div className="text-gray-500 dark:text-gray-200">Loading...</div>
-          )}
+    <Card className="mt-4 w-full truncate p-5 text-sm md:w-4/6" forceRounded>
+      <div className="flex flex-col gap-y-1">
+        <div className="flex items-center space-x-1.5">
+          <LinkIcon className="size-4 text-gray-500 dark:text-gray-200" />
+          <b className="max-w-sm truncate">{url}</b>
         </div>
-      </Card>
-    </div>
+        {!hideLoader && (
+          <div className="text-gray-500 dark:text-gray-200">Loading...</div>
+        )}
+      </div>
+    </Card>
   );
 };
 
