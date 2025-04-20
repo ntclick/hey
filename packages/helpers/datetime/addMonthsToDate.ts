afecc9c6bc0ba9c78f145ef4fdbfb475954e80ac
Yoginth <--global>
2025-04-20 16:11:30 +0530

    refactor: remove unused date helper functions for improved code clarity

--- packages/helpers/datetime/addMonthsToDate.ts
+++
@@ -1,7 +0,0 @@
-import dayjs from "dayjs";
-
-const addMonthsToDate = (date: Date | string, months: number) => {
-  return dayjs(new Date(date)).add(months, "month");
-};
-
-export default addMonthsToDate;
