1f3adddcda1eb15b1e4e4c6f6e2e929874061a99
Yoginth <yoginth@hey.com>
2025-04-21 09:08:28 +0530

    feat: enhance middleware to handle bot requests for /g/ paths and update routes to include /g/ in sitemap handling

--- apps/web/functions/_middleware.ts
+++ apps/web/functions/_middleware.ts
@@ -5,17 +5,14 @@ export const onRequest: any = async (context: any) => {
   const path = url.pathname;
 
   const isBot =
-    /(bot|telegram|baidu|bing|yandex|iframely|whatsapp|facebook)/.test(
-      userAgent
-    );
+    /bot|telegram|baidu|bing|yandex|iframely|whatsapp|facebook/.test(userAgent);
 
-  // Handle sitemap requests
   if (path === "/sitemap.xml" || path === "" || path.startsWith("/sitemap/")) {
-    let targetUrl: any;
+    let targetUrl: string;
 
-    if (path === "/sitemap.xml" || path === "") {
+    if (!path || path === "/sitemap.xml") {
       targetUrl = "https://api.hey.xyz/sitemap/all.xml";
-    } else if (path.startsWith("/sitemap/")) {
+    } else {
       const actualPath = path.replace("/sitemap/", "/");
       targetUrl = `https://api.hey.xyz/sitemap${actualPath}`;
     }
@@ -23,29 +20,27 @@ export const onRequest: any = async (context: any) => {
     const response = await fetch(targetUrl, {
       method: request.method,
       headers: request.headers,
-      body:
-        request.method !== "GET" && request.method !== "HEAD"
-          ? request.body
-          : undefined
+      body: ["GET", "HEAD"].includes(request.method) ? undefined : request.body
     });
 
     const newResponse = new Response(response.body, response);
     newResponse.headers.set("Cache-Control", "public, max-age=2592000");
-
     return newResponse;
   }
 
-  // Handle bot requests for OG data
-  if (isBot) {
-    if (path.startsWith("/u/") || path.startsWith("/posts/")) {
-      const target = `https://og.hey.xyz${path}`;
-      return fetch(target, {
-        headers: request.headers,
-        method: request.method,
-        body: request.body,
-        redirect: "follow"
-      });
-    }
+  if (
+    isBot &&
+    (path.startsWith("/u/") ||
+      path.startsWith("/posts/") ||
+      path.startsWith("/g/"))
+  ) {
+    const target = `https://og.hey.xyz${path}`;
+    return fetch(target, {
+      headers: request.headers,
+      method: request.method,
+      body: request.body,
+      redirect: "follow"
+    });
   }
 
   return context.next();
