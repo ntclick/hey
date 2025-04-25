75ebafa98db68994ef78ec73d7d666e0cbdd5a4d
Yoginth <hey@yoginth.com>
2025-04-25 11:49:44 +0530

    Refactor sitemap generation in sitemapIndex and accountsSitemapIndex

--- apps/api/src/routes/sitemap/sitemapIndex.ts
+++ apps/api/src/routes/sitemap/sitemapIndex.ts
@@ -11,12 +11,7 @@ const sitemapIndex = async (ctx: Context) => {
   try {
     const sitemapIndex = create({ version: "1.0", encoding: "UTF-8" }).ele(
       "sitemapindex",
-      {
-        "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
-        xmlns: "http://www.sitemaps.org/schemas/sitemap/0.9",
-        "xsi:schemaLocation":
-          "http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/siteindex.xsd"
-      }
+      { xmlns: "http://www.sitemaps.org/schemas/sitemap/0.9" }
     );
 
     for (const sitemap of sitemaps) {
