06c49dd67dcf6583fefbf703672ff74879a8843d
Yoginth <hey@yoginth.com>
2025-04-26 18:25:43 +0530

    Refactor infoLogger middleware to remove source information from logs, simplifying the output format

--- apps/api/src/middlewares/infoLogger.ts
+++ apps/api/src/middlewares/infoLogger.ts
@@ -1,22 +1,33 @@
 import type { Context, Next } from "hono";
 
+const getReadableUserAgent = (ua: string) => {
+  switch (true) {
+    case ua.includes("GPTBot"):
+      return "GPTBot";
+    case ua.includes("Googlebot"):
+      return "Googlebot";
+    case ua.includes("YandexBot"):
+      return "YandexBot";
+    default:
+      return "Other";
+  }
+};
+
 const infoLogger = async (c: Context, next: Next) => {
   const start = performance.now();
   const startMem = process.memoryUsage().heapUsed;
-
-  const source =
-    c.req.header("Origin") || c.req.header("User-Agent") || "unknown";
+  const ua = c.req.header("User-Agent") || "unknown";
+  const readableUa = getReadableUserAgent(ua);
 
   await next();
 
   const end = performance.now();
   const endMem = process.memoryUsage().heapUsed;
-
   const timeTakenMs = (end - start).toFixed(2);
   const memoryUsedMb = ((endMem - startMem) / 1024 / 1024).toFixed(2);
 
   console.info(
-    `[${c.req.method} ${c.req.path}] ➜ [${source}] ➜ ${timeTakenMs}ms, ${memoryUsedMb}mb`
+    `[${c.req.method} ${c.req.path}] ➜ [${readableUa}] ➜ ${timeTakenMs}ms, ${memoryUsedMb}mb`
   );
 };
 
