89f220da3789b0b4089ba19c68f7b88d1c69daaf
Yoginth <hey@yoginth.com>
2025-04-26 12:20:12 +0530

    Enhance originLogger middleware to include performance metrics for request handling

--- apps/api/src/middlewares/originLogger.ts
+++ apps/api/src/middlewares/originLogger.ts
@@ -4,13 +4,24 @@ const getIp = (req: HonoRequest) =>
   req.header("x-forwarded-for") || req.header("remote-addr") || "unknown";
 
 const originLogger = async (c: Context, next: Next) => {
+  const start = performance.now();
+  const startMem = process.memoryUsage().heapUsed;
+
   const source =
     c.req.header("Origin") || c.req.header("User-Agent") || "unknown";
   const ip = getIp(c.req);
-  console.info(
-    `[${c.req.method}] ➜ [${source}] with ip [${ip}] ➜ [${c.req.path}]`
-  );
+
   await next();
+
+  const end = performance.now();
+  const endMem = process.memoryUsage().heapUsed;
+
+  const timeTakenMs = (end - start).toFixed(2);
+  const memoryUsedMb = ((endMem - startMem) / 1024 / 1024).toFixed(2);
+
+  console.info(
+    `[${c.req.method}] ➜ [${source}] with ip [${ip}] ➜ [${c.req.path}] ➜ ${timeTakenMs}ms, ${memoryUsedMb}mb`
+  );
 };
 
 export default originLogger;
