2fe87da2ec96b16aabcee82a55853ef7bba773d2
Yoginth <hey@yoginth.com>
2025-04-28 10:53:17 +0530

    Enhance injectReferrerToUrl function to support dynamic referrer parameters based on domain

--- apps/web/src/helpers/injectReferrerToUrl.ts
+++ apps/web/src/helpers/injectReferrerToUrl.ts
@@ -1,7 +1,17 @@
 import { HEY_TREASURY } from "@hey/data/constants";
 
-const injectReferrerToUrl = (url: string, param = "referrer") => {
+const DOMAIN_PARAM_MAP: Record<string, string> = {
+  "zora.co": "referrer"
+};
+
+const injectReferrerToUrl = (url: string) => {
   const parsedUrl = new URL(url);
+  const param = DOMAIN_PARAM_MAP[parsedUrl.hostname];
+
+  if (!param) {
+    return url;
+  }
+
   parsedUrl.searchParams.set(param, HEY_TREASURY);
   return parsedUrl.toString();
 };
