30f0f7a132712f15ef7b86e3f7eb869bfc686818
Yoginth <hey@yoginth.com>
2025-05-02 13:49:39 +0530

    Refactor Features and Access mappings by removing the Pro feature for streamlined access control

--- packages/data/features.ts
+++ packages/data/features.ts
@@ -1,9 +1,7 @@
 export const Features = {
-  Staff: "staff",
-  Pro: "pro"
+  Staff: "staff"
 };
 
 export const Access = {
-  [Features.Staff]: ["0x51F9cb3CDd9434B1d56ed4d3291AF2CA0464feB0"],
-  [Features.Pro]: ["0x51F9cb3CDd9434B1d56ed4d3291AF2CA0464feB0"]
+  [Features.Staff]: ["0x51F9cb3CDd9434B1d56ed4d3291AF2CA0464feB0"]
 };
