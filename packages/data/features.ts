4f1be73798058455631424604772d48e84f01d8e
Yoginth <hey@yoginth.com>
2025-05-02 13:41:15 +0530

    Add Pro feature and update Access mapping for enhanced role management

--- packages/data/features.ts
+++ packages/data/features.ts
@@ -1,7 +1,9 @@
 export const Features = {
-  Staff: "staff"
+  Staff: "staff",
+  Pro: "pro"
 };
 
 export const Access = {
-  [Features.Staff]: ["0x51F9cb3CDd9434B1d56ed4d3291AF2CA0464feB0"]
+  [Features.Staff]: ["0x51F9cb3CDd9434B1d56ed4d3291AF2CA0464feB0"],
+  [Features.Pro]: ["0x51F9cb3CDd9434B1d56ed4d3291AF2CA0464feB0"]
 };
