22bc2cd6971906ecd4187a91eb3353b778c4f762
Yoginth <hey@yoginth.com>
2025-05-08 18:11:32 +0530

    Add more errors to ignoreErrors

--- apps/web/src/helpers/sentry.ts
+++ apps/web/src/helpers/sentry.ts
@@ -1,1 +1,4 @@
-export const ignoreErrors = ["Object captured as promise rejection with keys"];
+export const ignoreErrors = [
+  "Object captured as promise rejection with keys",
+  "No matching key. session topic doesn't exist" // From WalletConnect
+];
