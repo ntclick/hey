4c0b64f2d17a40e9653d3889ca17afd33c4fe231
Yoginth <hey@yoginth.com>
2025-05-12 14:45:54 +0530

    Added Pro accounts route to internal API

---
+++ apps/api/src/routes/internal/getProAccounts.ts
@@ -0,0 +1,33 @@
+import { Errors } from "@hey/data/errors";
+import type { Context } from "hono";
+import lensPg from "src/utils/lensPg";
+
+const getProAccounts = async (ctx: Context) => {
+  try {
+    const proAccounts = await lensPg.query(
+      `
+        SELECT DISTINCT aksw.owned_by
+        FROM post.action_executed pae
+        JOIN account.known_smart_wallet aksw
+          ON pae.account = aksw.address
+        WHERE pae.post_id = '\\x001c62d4107cdb7d7508146ca1aa6b289d6bb5d41adb6455df747153334669ba'
+          AND pae.timestamp >= NOW() - INTERVAL '30 days'
+          AND pae.type = 'TippingPostAction'
+          AND pae.decoded_params->>'value' = '0x29a2241af62c0000'
+          AND pae.decoded_params->>'currency' = '0x6bdc36e20d267ff0dd6097799f82e78907105e2f';
+      `
+    );
+
+    // Convert Buffer to hex string
+    const accounts = proAccounts.map(
+      (account) => `0x${account.owned_by.toString("hex")}`
+    );
+
+    return ctx.text(accounts.join("\n"));
+  } catch (e) {
+    console.error(e);
+    return ctx.json({ success: false, error: Errors.SomethingWentWrong }, 500);
+  }
+};
+
+export default getProAccounts;
--- apps/api/src/routes/internal/index.ts
+++ apps/api/src/routes/internal/index.ts
@@ -1,8 +1,10 @@
 import { Hono } from "hono";
+import getProAccounts from "./getProAccounts";
 import permissionRouter from "./permission";
 
 const app = new Hono();
 
 app.route("/permission", permissionRouter);
+app.get("/pro", getProAccounts);
 
 export default app;
