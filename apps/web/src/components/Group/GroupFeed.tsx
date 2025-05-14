e69213d2477ec93897e87283de131bb179298e28
Yoginth <hey@yoginth.com>
2025-05-14 18:17:08 +0530

    Enhance GroupFeed component by improving post filtering logic to exclude reported posts and adding type safety for post items

--- apps/web/src/components/Group/GroupFeed.tsx
+++ apps/web/src/components/Group/GroupFeed.tsx
@@ -2,7 +2,12 @@ import SinglePost from "@/components/Post/SinglePost";
 import PostsShimmer from "@/components/Shared/Shimmer/PostsShimmer";
 import { Card, EmptyState, ErrorMessage } from "@/components/Shared/UI";
 import { ChatBubbleBottomCenterIcon } from "@heroicons/react/24/outline";
-import { PageSize, type PostsRequest, usePostsQuery } from "@hey/indexer";
+import {
+  PageSize,
+  type PostFragment,
+  type PostsRequest,
+  usePostsQuery
+} from "@hey/indexer";
 import { useIntersectionObserver } from "@uidotdev/usehooks";
 import { useEffect } from "react";
 import { WindowVirtualizer } from "virtua";
@@ -28,7 +33,7 @@ const GroupFeed = ({ feed }: GroupFeedProps) => {
     variables: { request }
   });
 
-  const posts = data?.posts?.items;
+  const posts = data?.posts?.items as PostFragment[];
   const pageInfo = data?.posts?.pageInfo;
   const hasMore = pageInfo?.next;
 
@@ -69,8 +74,9 @@ const GroupFeed = ({ feed }: GroupFeedProps) => {
         {posts
           .filter(
             (post) =>
-              !post.author.operations?.hasBlockedMe ||
-              !post.author.operations?.isBlockedByMe
+              !post.author.operations?.hasBlockedMe &&
+              !post.author.operations?.isBlockedByMe &&
+              !post.operations?.hasReported
           )
           .map((post, index) => (
             <SinglePost
