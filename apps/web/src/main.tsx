c662603e1a3c42e3a0795766185f132dd91d6f46
Yoginth <hey@yoginth.com>
2025-05-15 17:26:40 +0530

    Remove service worker unregistration code from main.tsx

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -6,15 +6,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Routes } from "./routes";
 
-// TODO: Remove this after the one month
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.getRegistrations().then((registrations) => {
-    for (const registration of registrations) {
-      registration.unregister();
-    }
-  });
-}
-
 createRoot(document.getElementById("_hey_") as HTMLElement).render(
   <StrictMode>
     <Providers>
