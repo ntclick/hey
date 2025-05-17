ec2a825e7503010a5dfd898da2222d7794d4b837
Yoginth <hey@yoginth.com>
2025-05-17 16:05:10 +0530

    Enhance jumper endpoints to accept ID parameters with Zod validation

--- apps/api/src/routes/temp/index.ts
+++ apps/api/src/routes/temp/index.ts
@@ -1,10 +1,19 @@
+import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import getJumperQuotes from "./getJumperQuotes";
 import getJumperTips from "./getJumperTips";
 
 const app = new Hono();
 
-app.get("/jumper/quotes", getJumperQuotes);
-app.get("/jumper/tips", getJumperTips);
+app.get(
+  "/jumper/quotes/:id",
+  zValidator("param", z.object({ id: z.string() })),
+  getJumperQuotes
+);
+app.get(
+  "/jumper/tips/:id",
+  zValidator("param", z.object({ id: z.string() })),
+  getJumperTips
+);
 
 export default app;
