0aaf0938768d3a44529637596666dae40eefa49c
Yoginth <hey@yoginth.com>
2025-05-17 16:03:41 +0530

    Add endpoint for jumper tips in tempRouter

--- apps/api/src/routes/temp/index.ts
+++ apps/api/src/routes/temp/index.ts
@@ -1,8 +1,10 @@
 import { Hono } from "hono";
 import getJumperQuotes from "./getJumperQuotes";
+import getJumperTips from "./getJumperTips";
 
 const app = new Hono();
 
 app.get("/jumper/quotes", getJumperQuotes);
+app.get("/jumper/tips", getJumperTips);
 
 export default app;
