ff58cb6332054196a7a56bce9b15cbc10219d03a
Yoginth <hey@yoginth.com>
2025-06-05 12:25:44 +0530

    Refactor getMentions helper (#5645)

--- apps/web/src/helpers/getMentions.ts
+++ apps/web/src/helpers/getMentions.ts
@@ -1,27 +1,22 @@
 import { Regex } from "@hey/data/regex";
 import type { AccountMentionFragment, PostMentionFragment } from "@hey/indexer";
 
-const getMentions = (text: string): [] | PostMentionFragment[] => {
-  if (!text) {
-    return [];
-  }
+const getMentions = (text: string): PostMentionFragment[] => {
+  if (!text) return [];
 
-  const mentions = text.match(Regex.mention);
-  const processedMentions = mentions?.map((mention) => {
-    const splited = mention.split("/");
-    const handleWithoutNameSpace = splited[splited.length - 1];
+  const mentions = text.match(Regex.mention) ?? [];
+
+  return mentions.map((mention) => {
+    const handle = mention
+      .substring(mention.lastIndexOf("/") + 1)
+      .toLowerCase();
 
     return {
       account: "",
       namespace: "",
-      replace: {
-        from: handleWithoutNameSpace.toLowerCase(),
-        to: handleWithoutNameSpace.toLowerCase()
-      }
+      replace: { from: handle, to: handle }
     } as AccountMentionFragment;
   });
-
-  return processedMentions || [];
 };
 
 export default getMentions;
