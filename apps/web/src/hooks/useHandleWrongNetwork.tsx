af0f66ab8e3bd35e13cd2a65a3ef8d5e7ac92030
Yoginth <hey@yoginth.com>
2025-06-05 12:11:29 +0530

    Refactor useHandleWrongNetwork (#5638)

--- apps/web/src/hooks/useHandleWrongNetwork.tsx
+++ apps/web/src/hooks/useHandleWrongNetwork.tsx
@@ -1,29 +1,26 @@
 import { CHAIN } from "@hey/data/constants";
-import { useConnections, useSwitchChain } from "wagmi";
+import { useCallback } from "react";
+import { useAccount, useChainId, useSwitchChain } from "wagmi";
 
 const useHandleWrongNetwork = () => {
-  const activeConnection = useConnections();
+  const { isConnected } = useAccount();
+  const chainId = useChainId();
   const { switchChainAsync } = useSwitchChain();
 
-  const isConnected = () => activeConnection[0] !== undefined;
-  const isWrongNetwork = () => activeConnection[0]?.chainId !== CHAIN.id;
-
-  const handleWrongNetwork = async () => {
-    if (!isConnected()) {
+  return useCallback(async () => {
+    if (!isConnected) {
       console.warn("No active connection found.");
       return;
     }
 
-    if (isWrongNetwork()) {
+    if (chainId !== CHAIN.id) {
       try {
         await switchChainAsync({ chainId: CHAIN.id });
       } catch (error) {
         console.error("Failed to switch chains:", error);
       }
     }
-  };
-
-  return handleWrongNetwork;
+  }, [chainId, isConnected, switchChainAsync]);
 };
 
 export default useHandleWrongNetwork;
