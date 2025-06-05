d1f9a856d7c8277a68c3d33c9be1fafc9e2a9114
Yoginth <hey@yoginth.com>
2025-06-05 12:29:49 +0530

    Refactor nFormatter utility (#5649)

--- apps/web/src/helpers/nFormatter.ts
+++ apps/web/src/helpers/nFormatter.ts
@@ -1,25 +1,35 @@
 import humanize from "@/helpers/humanize";
 
+const LOOKUP = [
+  { value: 1e18, symbol: "E" },
+  { value: 1e15, symbol: "P" },
+  { value: 1e12, symbol: "T" },
+  { value: 1e9, symbol: "G" },
+  { value: 1e6, symbol: "M" },
+  { value: 1e3, symbol: "k" },
+  { value: 1, symbol: "" }
+];
+
+const TRIM_ZEROES_REGEX = /\.0+$|(\.\d*[1-9])0+$/;
+
 const nFormatter = (num: number, digits = 1): string => {
-  if (!Number.isFinite(num)) return "";
+  if (!Number.isFinite(num)) {
+    return "";
+  }
 
-  const lookup = [
-    { value: 1e18, symbol: "E" },
-    { value: 1e15, symbol: "P" },
-    { value: 1e12, symbol: "T" },
-    { value: 1e9, symbol: "G" },
-    { value: 1e6, symbol: "M" },
-    { value: 1e3, symbol: "k" },
-    { value: 1, symbol: "" }
-  ];
-  const rx = /\.0+$|(\.\d*[1-9])0+$/;
+  if (num < 1000) {
+    return humanize(num);
+  }
 
-  const item = lookup.find((i) => num >= i.value);
+  const item = LOOKUP.find((i) => num >= i.value);
+  if (!item) {
+    return "0";
+  }
 
-  if (!item) return "0";
-  if (num < 1000) return humanize(num);
-
-  return (num / item.value).toFixed(digits).replace(rx, "$1") + item.symbol;
+  return (num / item.value)
+    .toFixed(digits)
+    .replace(TRIM_ZEROES_REGEX, "$1")
+    .concat(item.symbol);
 };
 
 export default nFormatter;
