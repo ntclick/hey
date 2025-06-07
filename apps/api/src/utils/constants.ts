bc20be21ca6f29fbbf9a7bb89317f42ef973124a
Yoginth <hey@yoginth.com>
2025-06-07 12:15:05 +0530

    Remove unused constant (#5670)

--- apps/api/src/utils/constants.ts
+++ apps/api/src/utils/constants.ts
@@ -1,5 +1,4 @@
 export const HEY_USER_AGENT = "HeyBot (like TwitterBot)";
-export const VERIFICATION_ENDPOINT = "https://api.hey.xyz/lens/verification";
 export const SITEMAP_BATCH_SIZE = 25000;
 // Cache settings for different durations
 export const CACHE_AGE_1_DAY = "public, s-maxage=86400, max-age=86400"; // Cache for 1 day
