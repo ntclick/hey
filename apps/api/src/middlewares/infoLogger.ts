1ef4732ce54e6d5e26531bfec5cf5aeb2b1c6954
Yoginth <hey@yoginth.com>
2025-06-07 13:29:28 +0530

    Add detailed logging toggle (#5673)

--- apps/api/src/middlewares/infoLogger.ts
+++ apps/api/src/middlewares/infoLogger.ts
@@ -15,9 +15,10 @@ const getReadableUserAgent = (ua: string) => {
 
 const infoLogger = async (c: Context, next: Next) => {
   const start = performance.now();
-  const startMem = process.memoryUsage().heapUsed;
   const ua = c.req.header("User-Agent") || "unknown";
   const readableUa = getReadableUserAgent(ua);
+
+  const startMem = process.memoryUsage().heapUsed;
 
   await next();
 
@@ -25,10 +26,9 @@ const infoLogger = async (c: Context, next: Next) => {
   const endMem = process.memoryUsage().heapUsed;
   const timeTakenMs = (end - start).toFixed(2);
   const memoryUsedMb = ((endMem - startMem) / 1024 / 1024).toFixed(2);
+  const message = `[${c.req.method} ${c.req.path}] ➜ [${readableUa}] ➜ ${timeTakenMs}ms, ${memoryUsedMb}mb`;
 
-  console.info(
-    `[${c.req.method} ${c.req.path}] ➜ [${readableUa}] ➜ ${timeTakenMs}ms, ${memoryUsedMb}mb`
-  );
+  console.info(message);
 };
 
 export default infoLogger;
