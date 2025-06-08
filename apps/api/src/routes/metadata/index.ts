0435505289e0afd5e49623e6c740869594b20482
Yoginth <hey@yoginth.com>
2025-06-08 20:57:19 +0530

    Add auth middleware to STS route (#5729)

--- apps/api/src/routes/metadata/index.ts
+++ apps/api/src/routes/metadata/index.ts
@@ -1,9 +1,10 @@
 import { Hono } from "hono";
+import authMiddleware from "src/middlewares/authMiddleware";
 import rateLimiter from "src/middlewares/rateLimiter";
 import getSTS from "./getSTS";
 
 const app = new Hono();
 
-app.get("/sts", rateLimiter({ requests: 50 }), getSTS);
+app.get("/sts", rateLimiter({ requests: 50 }), authMiddleware, getSTS);
 
 export default app;
