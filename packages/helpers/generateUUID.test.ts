b56bad760a37d9f2888c5eeaea8e938c70d11958
Yoginth <hey@yoginth.com>
2025-06-08 13:19:03 +0530

    Add UUID uniqueness check (#5710)

--- packages/helpers/generateUUID.test.ts
+++ packages/helpers/generateUUID.test.ts
@@ -8,4 +8,16 @@ describe("generateUUID", () => {
       /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/
     );
   });
+
+  it("creates unique v4 UUIDs", () => {
+    const regex =
+      /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/;
+    const uuids = new Set<string>();
+    for (let i = 0; i < 100; i += 1) {
+      const uuid = generateUUID();
+      expect(uuid).toMatch(regex);
+      uuids.add(uuid);
+    }
+    expect(uuids.size).toBe(100);
+  });
 });
