7ec681db9a00b498bae58da7bf28584f9093e09b
Yoginth <hey@yoginth.com>
2025-06-10 22:39:43 +0530

    Remove syncHQScoreAccountsToGuild

--- apps/api/src/routes/cron/index.ts
+++ apps/api/src/routes/cron/index.ts
@@ -1,18 +1,12 @@
 import { Hono } from "hono";
 import secretMiddleware from "src/middlewares/secretMiddleware";
 import syncFollowersStandingToGuild from "./guild/syncFollowersStandingToGuild";
-import syncHQScoreAccountsToGuild from "./guild/syncHQScoreAccountsToGuild";
 import syncSubscribersToGuild from "./guild/syncSubscribersToGuild";
 import removeExpiredSubscribers from "./removeExpiredSubscribers";
 
 const app = new Hono();
 
 app.get("/syncSubscribersToGuild", secretMiddleware, syncSubscribersToGuild);
-app.get(
-  "/syncHQScoreAccountsToGuild",
-  secretMiddleware,
-  syncHQScoreAccountsToGuild
-);
 app.get(
   "/syncFollowersStandingToGuild",
   secretMiddleware,
