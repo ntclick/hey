e07c301afd0bb8ad97b293495895448a7dc4276b
Yoginth <hey@yoginth.com>
2025-06-10 10:19:02 +0530

    refactor: Simplify infoLogger middleware by removing user agent parsing and enhancing log message format

--- apps/api/src/middlewares/infoLogger.ts
+++ apps/api/src/middlewares/infoLogger.ts
@@ -1,23 +1,7 @@
 import type { Context, Next } from "hono";
 
-const getReadableUserAgent = (ua: string) => {
-  switch (true) {
-    case ua.includes("GPTBot"):
-      return "GPTBot";
-    case ua.includes("Googlebot"):
-      return "Googlebot";
-    case ua.includes("YandexBot"):
-      return "YandexBot";
-    default:
-      return "Other";
-  }
-};
-
 const infoLogger = async (c: Context, next: Next) => {
   const start = performance.now();
-  const ua = c.req.header("User-Agent") || "unknown";
-  const readableUa = getReadableUserAgent(ua);
-
   const startMem = process.memoryUsage().heapUsed;
 
   await next();
@@ -26,7 +10,7 @@ const infoLogger = async (c: Context, next: Next) => {
   const endMem = process.memoryUsage().heapUsed;
   const timeTakenMs = (end - start).toFixed(2);
   const memoryUsedMb = ((endMem - startMem) / 1024 / 1024).toFixed(2);
-  const message = `[${c.req.method} ${c.req.path}] ➜ [${readableUa}] ➜ ${timeTakenMs}ms, ${memoryUsedMb}mb`;
+  const message = `[${c.req.method} ${c.req.path}] ➜ ${timeTakenMs}ms, ${memoryUsedMb}mb`;
 
   console.info(message);
 };
