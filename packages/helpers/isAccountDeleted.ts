9f31d47978055a4fe83e810936fff5c021262201
Yoginth <hey@yoginth.com>
2025-06-14 22:11:49 +0530

    refactor: simplify account deletion check (#5782)

--- packages/helpers/isAccountDeleted.ts
+++ packages/helpers/isAccountDeleted.ts
@@ -1,12 +1,7 @@
 import { NULL_ADDRESS } from "@hey/data/constants";
 import type { AccountFragment } from "@hey/indexer";
 
-const isAccountDeleted = (account: AccountFragment): boolean => {
-  if (account.owner === NULL_ADDRESS) {
-    return true;
-  }
-
-  return false;
-};
+const isAccountDeleted = (account: AccountFragment): boolean =>
+  account.owner === NULL_ADDRESS;
 
 export default isAccountDeleted;
