fe1d4ede942b658170698098207c40327498e357
Yoginth <hey@yoginth.com>
2025-06-14 22:07:00 +0530

    Fix getFavicon return type (#5775)

--- apps/web/src/helpers/getFavicon.ts
+++ apps/web/src/helpers/getFavicon.ts
@@ -2,16 +2,15 @@ const FAVICON_BASE_URL = "https://external-content.duckduckgo.com/ip3";
 const UNKNOWN_DOMAIN = "unknowndomain";
 
 const getFavicon = (url: string): string => {
-  if (!url) {
-    return `${FAVICON_BASE_URL}/${UNKNOWN_DOMAIN}.ico`;
-  }
+  const domain = (() => {
+    try {
+      return new URL(url).hostname;
+    } catch {
+      return UNKNOWN_DOMAIN;
+    }
+  })();
 
-  try {
-    const { hostname } = new URL(url);
-    return `${FAVICON_BASE_URL}/${hostname || UNKNOWN_DOMAIN}.ico`;
-  } catch {
-    return `${FAVICON_BASE_URL}/${UNKNOWN_DOMAIN}.ico`;
-  }
+  return `${FAVICON_BASE_URL}/${domain || UNKNOWN_DOMAIN}.ico`;
 };
 
 export default getFavicon;
