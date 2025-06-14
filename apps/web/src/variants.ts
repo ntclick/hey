d7aaa80399bb79dd636a4cc7b9db18ab512ff44f
Yoginth <hey@yoginth.com>
2025-06-14 18:56:43 +0530

    Update dependencies (#5756)

--- apps/web/src/variants.ts
+++ apps/web/src/variants.ts
@@ -3,8 +3,8 @@ export const accountsList = {
   visible: {
     opacity: 1,
     y: 0,
-    transition: { duration: 0.3, ease: [0.42, 0, 0.58, 1] }
+    transition: { duration: 0.3, ease: [0.42, 0, 0.58, 1] as const }
   }
 };
 
-export const EXPANSION_EASE = [0.075, 0.82, 0.165, 1];
+export const EXPANSION_EASE = [0.075, 0.82, 0.165, 1] as const;
