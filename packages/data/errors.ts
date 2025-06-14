c8432ca862d40c55b903114b4c711e75d161cf5a
Yoginth <hey@yoginth.com>
2025-06-14 22:02:36 +0530

    Remove unused export of ErrorMessage type from errors.ts

--- packages/data/errors.ts
+++ packages/data/errors.ts
@@ -2,5 +2,3 @@ export const ERRORS = {
   SignWallet: "Please sign in your wallet.",
   SomethingWentWrong: "Something went wrong!"
 } as const;
-
-export type ErrorMessage = (typeof ERRORS)[keyof typeof ERRORS];
