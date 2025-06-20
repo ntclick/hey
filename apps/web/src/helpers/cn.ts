dd8c9f0182cc933b49c44614f62b42b954360574
Yoginth <hey@yoginth.com>
2025-06-20 12:47:20 +0530

    refactor: simplify cn helper (#5805)

--- apps/web/src/helpers/cn.ts
+++ apps/web/src/helpers/cn.ts
@@ -2,8 +2,6 @@ import type { ClassValue } from "clsx";
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-const cn = (...inputs: ClassValue[]) => {
-  return twMerge(clsx(inputs));
-};
+const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
 
 export default cn;
