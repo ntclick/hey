fc17c0f354d3429e907e9a27e4a8ed59f577d4ed
Yoginth <hey@yoginth.com>
2025-06-20 13:06:27 +0530

    chore: remove vitest (#5814)

--- apps/web/src/helpers/trimify.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, expect, it } from "vitest";
-import trimify from "./trimify";
-
-describe("trimify", () => {
-  it("removes extra newlines and trims", () => {
-    const input = "a\n\n\n b \n";
-    expect(trimify(input)).toBe("a\n\n b");
-  });
-});
