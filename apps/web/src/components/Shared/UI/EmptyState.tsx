5a069c1e2e600afc8c5b0cb7b578fe741c7bcfb9
Yoginth <hey@yoginth.com>
2025-06-29 11:54:45 +0530

    feat: add variants for EmptyState (#5885)

--- apps/web/src/components/Shared/UI/EmptyState.tsx
+++ apps/web/src/components/Shared/UI/EmptyState.tsx
@@ -1,6 +1,6 @@
+import { cva } from "class-variance-authority";
 import { memo, type ReactNode } from "react";
 import { Card } from "@/components/Shared/UI";
-import cn from "@/helpers/cn";
 
 interface EmptyStateProps {
   hideCard?: boolean;
@@ -9,6 +9,16 @@ interface EmptyStateProps {
   className?: string;
 }
 
+const emptyStateVariants = cva("", {
+  defaultVariants: { hideCard: false },
+  variants: {
+    hideCard: {
+      false: "",
+      true: "!bg-transparent !shadow-none !border-0"
+    }
+  }
+});
+
 const EmptyState = ({
   hideCard = false,
   icon,
@@ -16,12 +26,7 @@ const EmptyState = ({
   className = ""
 }: EmptyStateProps) => {
   return (
-    <Card
-      className={cn(
-        { "!bg-transparent !shadow-none !border-0": hideCard },
-        className
-      )}
-    >
+    <Card className={emptyStateVariants({ className, hideCard })}>
       <div className="grid justify-items-center space-y-2 p-5">
         <div>{icon}</div>
         <div>{message}</div>
